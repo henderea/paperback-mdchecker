49b51ad9533fdd54f325a9243ce59e106fb844ad
Eric Henderson <henderea@gmail.com>
2023-08-08 14:06:27 -0400

    fix some issues with the database setup

--- tables.sql
+++ tables.sql
@@ -1,4 +1,4 @@
-create table user (
+create table user_id (
   user_id text not null,
   constraint pk_user primary key (user_id)
 );
@@ -6,10 +6,10 @@ create table user (
 create table user_manga (
   user_id text not null,
   manga_id text not null,
-  last_check number,
-  last_update number,
+  last_check integer,
+  last_update integer,
   constraint pk_user_manga primary key (user_id, manga_id),
-  constraint fk_user_manga_user_id foreign key (user_id) references user (user_id)
+  constraint fk_user_manga_user_id foreign key (user_id) references user_id (user_id)
 );
 
 create index ix_user_manga_manga_id_last_check on user_manga (manga_id, last_check);
