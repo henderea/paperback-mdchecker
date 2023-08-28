f2916fcfdc1362c276b5fcca42cebc5314d4b87e
Eric Henderson <henderea@gmail.com>
2023-08-28 10:01:42 -0400

    increase the window

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ app.get('/last-update-check', async (req, res) => {
     if(lastUserCheck > 0) {
       const lastUserCheckTime = new Date(lastUserCheck);
       userData.lastUserFetch = formatDate(lastUserCheckTime);
-      const userUpdateCount = await getUserUpdateCount(userId, lastUserCheck - Duration.HOURS(2));
+      const userUpdateCount = await getUserUpdateCount(userId, lastUserCheck - Duration.HOURS(6));
       userData.updatesSinceLastFetch = userUpdateCount;
     }
     const lastCheck = await getLatestUpdateCheck();
