a4d86a70f85fbab851c7214cbc6a0982404a2eb3
Eric Henderson <henderea@gmail.com>
2023-09-06 16:26:34 -0400

    split out the new PushoverUser and PushoverUserList into a separate file

---
+++ lib/PushoverUserList.ts
@@ -0,0 +1,47 @@
+import type { UserResult } from './db';
+import { listUsers } from './db';
+import { User } from './UserList';
+
+export class PushoverUser extends User {
+  private readonly _pushoverToken: string | null;
+  private readonly _pushoverAppTokenOverride: string | null;
+
+  constructor(row: UserResult) {
+    super(row);
+    this._pushoverToken = row.pushover_token;
+    this._pushoverAppTokenOverride = row.pushover_app_token_override;
+  }
+
+  get pushoverToken(): string | null { return this._pushoverToken; }
+  get pushoverAppTokenOverride(): string | null { return this._pushoverAppTokenOverride; }
+  get hasPushover(): boolean { return !!this.pushoverToken; }
+  getPushoverAppToken(token: string | null): string | null { return this.pushoverAppTokenOverride ?? token; }
+}
+
+function processPushoverUsers(data: UserResult[]): Dictionary<PushoverUser> {
+  const rv: Dictionary<PushoverUser> = {};
+  data.forEach((row) => {
+    const user = new PushoverUser(row);
+    rv[user.userId] = user;
+  });
+  return rv;
+}
+
+export class PushoverUserList {
+  private _users: Dictionary<PushoverUser> = {};
+
+  private get users(): Dictionary<PushoverUser> { return this._users; }
+  private set users(users: Dictionary<PushoverUser>) { this._users = users; }
+
+  getUser(userId: string | null | undefined): PushoverUser | undefined { return userId ? this.users[userId] : undefined; }
+
+  hasUser(userId: string | null | undefined): boolean { return !!this.getUser(userId); }
+
+  async update(): Promise<void> {
+    try {
+      this.users = processPushoverUsers(await listUsers());
+    } catch (e) {
+      console.error('Encountered error updating users', e);
+    }
+  }
+}
--- lib/UserList.ts
+++ lib/UserList.ts
@@ -1,5 +1,5 @@
-import type { BasicUserResult, UserResult } from './db';
-import { listUsers, listUsersBasic } from './db';
+import type { BasicUserResult } from './db';
+import { listUsersBasic } from './db';
 
 export const roles = ['ADMIN'] as const;
 export type Role = typeof roles[number];
@@ -24,35 +24,10 @@ export class User {
   async ifAdminP<T>(value: () => Promise<T>): Promise<T | undefined> { return this.isAdmin ? await value() : undefined; }
 }
 
-export class PushoverUser extends User {
-  private readonly _pushoverToken: string | null;
-  private readonly _pushoverAppTokenOverride: string | null;
-
-  constructor(row: UserResult) {
-    super(row);
-    this._pushoverToken = row.pushover_token;
-    this._pushoverAppTokenOverride = row.pushover_app_token_override;
-  }
-
-  get pushoverToken(): string | null { return this._pushoverToken; }
-  get pushoverAppTokenOverride(): string | null { return this._pushoverAppTokenOverride; }
-  get hasPushover(): boolean { return !!this.pushoverToken; }
-  getPushoverAppToken(token: string | null): string | null { return this.pushoverAppTokenOverride ?? token; }
-}
-
 function processUsers(data: BasicUserResult[]): Dictionary<User> {
   const rv: Dictionary<User> = {};
   data.forEach((row) => {
     const user = new User(row);
-    rv[user.userId] = user;
-  });
-  return rv;
-}
-
-function processPushoverUsers(data: UserResult[]): Dictionary<PushoverUser> {
-  const rv: Dictionary<PushoverUser> = {};
-  data.forEach((row) => {
-    const user = new PushoverUser(row);
     rv[user.userId] = user;
   });
   return rv;
@@ -71,25 +46,6 @@ export class UserList {
   async update(): Promise<void> {
     try {
       this.users = processUsers(await listUsersBasic());
-    } catch (e) {
-      console.error('Encountered error updating users', e);
-    }
-  }
-}
-
-export class PushoverUserList {
-  private _users: Dictionary<PushoverUser> = {};
-
-  private get users(): Dictionary<PushoverUser> { return this._users; }
-  private set users(users: Dictionary<PushoverUser>) { this._users = users; }
-
-  getUser(userId: string | null | undefined): PushoverUser | undefined { return userId ? this.users[userId] : undefined; }
-
-  hasUser(userId: string | null | undefined): boolean { return !!this.getUser(userId); }
-
-  async update(): Promise<void> {
-    try {
-      this.users = processPushoverUsers(await listUsers());
     } catch (e) {
       console.error('Encountered error updating users', e);
     }
