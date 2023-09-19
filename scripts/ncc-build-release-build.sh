e36f64c86bdbcff789eb3b93bee5421d2c39b1a5
Eric Henderson <henderea@gmail.com>
2023-09-19 17:20:49 -0400

    build the source map; hopefully this will make stack traces better

--- scripts/ncc-build-release-build.sh
+++ scripts/ncc-build-release-build.sh
@@ -6,6 +6,6 @@ ncc="$(yarn bin ncc)"
 for s; do
   s="$(_cleanup_param "$s")"
   bin_path="$(_get_bin_path "$s")"
-  _pcmd "ncc build $bin_path -m -q -o build/$s ${EXCLUDES[*]}"
-  "$ncc" build "$bin_path" -m -q -o "build/$s" ${EXCLUDES[@]}
+  _pcmd "ncc build $bin_path -m -q -s -o build/$s ${EXCLUDES[*]}"
+  "$ncc" build "$bin_path" -m -q -s -o "build/$s" ${EXCLUDES[@]}
 done
