c45a2cde54c740290955c21ce126d2b2b6ae0de0
Eric Henderson <henderea@gmail.com>
2024-02-19 18:31:58 -0500

    do this the npm way

--- scripts/ncc-build-release-build.sh
+++ scripts/ncc-build-release-build.sh
@@ -2,10 +2,9 @@
 
 source "$(dirname "$0")/_util.sh"
 
-ncc="$(yarn bin ncc)"
 for s; do
   s="$(_cleanup_param "$s")"
   bin_path="$(_get_bin_path "$s")"
   _pcmd "ncc build $bin_path -m -q -o build/$s ${EXCLUDES[*]}"
-  "$ncc" build "$bin_path" -m -q -o "build/$s" ${EXCLUDES[@]}
+  npx @vercel/ncc build "$bin_path" -m -q -o "build/$s" ${EXCLUDES[@]}
 done
