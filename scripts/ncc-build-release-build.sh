a6489b0d9b6042703b2abf844492e4e9a313ce50
Eric Henderson <henderea@gmail.com>
2024-02-24 13:09:38 -0500

    switch to pnpm

--- scripts/ncc-build-release-build.sh
+++ scripts/ncc-build-release-build.sh
@@ -2,9 +2,10 @@
 
 source "$(dirname "$0")/_util.sh"
 
+ncc="$(pnpm bin)/ncc"
 for s; do
   s="$(_cleanup_param "$s")"
   bin_path="$(_get_bin_path "$s")"
   _pcmd "ncc build $bin_path -m -q -o build/$s ${EXCLUDES[*]}"
-  npx @vercel/ncc build "$bin_path" -m -q -o "build/$s" ${EXCLUDES[@]}
+  "$ncc" build "$bin_path" -m -q -o "build/$s" ${EXCLUDES[@]}
 done
