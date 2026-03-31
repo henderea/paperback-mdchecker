238ad1ec1469e544c51dd9d47f548de3d64799f3
Eric Henderson <henderea@gmail.com>
2026-03-31 13:17:32 -0400

    fix typescript issues

--- ts-types/global.d.ts
+++ ts-types/global.d.ts
@@ -1,13 +1,15 @@
 declare type Optional<T> = T | null | undefined;
 declare type List<T> = ArrayLike<T>;
-declare type Many<T> = T | ReadonlyArray<T>;
+declare type Many<T> = T | readonly T[];
 declare type EmptyObject = Record<string, never>;
 
 declare interface Dictionary<T> {
   [index: string]: T;
 }
 
-declare module 'got' {
-  import * as got from 'got/dist/source';
-  export = got;
-}
+// declare module 'got' {
+//   import * as got from 'got/dist/source';
+//   export = got;
+// }
+
+declare module '*.scss' {}
